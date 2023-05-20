82bc037f5c7724d7204171f6d681f143ad838e6f
thenabiev <sahinnabee@gmail.com>
2023-05-20 17:21:01 +0400

    .

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -7,6 +7,7 @@ import SponsorForbes from '@/assets/SponsorForbes.png';
 import SponsorFortune from '@/assets/SponsorFortune.png';
 import SponsorRedBull from '@/assets/SponsorRedBull.png';
 import HomePageGraphic from '@/assets/HomePageGraphic.png';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 
 
@@ -22,13 +23,13 @@ const Home = ({setSelectedPage}: Props) => {
       className='gap-16 bg-gray-20 py-10 md:h-full md:pb-0'
     >
       {/* Image and Main Header */}
-      <div>
+      <div className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6'>
         {/* Main Header */}
-        <div>
+        <div className='z-10 mt-32 md:basis-3/5'>
           {/* Headings */}
-          <div>
-            <div>
-              <div>
+          <div className='md:-mt-20'>
+            <div className='relative'>
+              <div className='before:absolute before:-top-20 md:before:content-evolvetext before:z-[-1] before:-left-20'>
                 <img src={HomePageText} alt="Home page text" />
               </div>
             </div>
@@ -39,12 +40,34 @@ const Home = ({setSelectedPage}: Props) => {
             {/* Actions */}
             <div>
               <ActionButton setSelectedPage={setSelectedPage}>Join Now</ActionButton>
+              <AnchorLink
+                className='text-sm font-bold text-primary-500 underline hover:text-secondary-500'
+                onClick={()=>setSelectedPage(SelectedPage.Contact)}
+                href={`#${SelectedPage.Contact}`}
+              >
+                <p>Learn More</p>
+              </AnchorLink>
             </div>
         </div>
 
         {/* Image */}
-        <div></div>
+        <div>
+          <img src={HomePageGraphic} alt="Home page graphic" />
+        </div>
       </div>
+
+      {/* Sponsors */}
+      {isAboveMediumScreens&&(
+        <div>
+          <div>
+            <div>
+              <img src={SponsorRedBull} alt="Redbull" />
+              <img src={SponsorForbes} alt="Forbes" />
+              <img src={SponsorFortune} alt="Fortune" />
+            </div>
+          </div>
+        </div>
+      )}
     </section>
   )
 }
